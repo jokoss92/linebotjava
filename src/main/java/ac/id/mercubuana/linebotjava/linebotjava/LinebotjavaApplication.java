60e2ac028b1b7d4d7ed1620e5388df6a314c8ea9
Joko Sarjono S <johno.smakaduta@gmail.com>
2020-03-22 23:56:23 +0700

    Initial commit

---
+++ src/main/java/ac/id/mercubuana/linebotjava/linebotjava/LinebotjavaApplication.java
@@ -0,0 +1,13 @@
+package ac.id.mercubuana.linebotjava.linebotjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinebotjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinebotjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ac/id/mercubuana/linebotjava/linebotjava/LinebotjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.id.mercubuana.linebotjava.linebotjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinebotjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
